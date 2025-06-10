508774089620fa8017e64863b2b53dec2de3323b
Jesus Francisco CAstrejón Canales <franck_768@hotmail.com>
2025-06-10 16:56:00 -0600

    format code

--- app/api/fibo/route.ts
+++ app/api/fibo/route.ts
@@ -1,16 +1,13 @@
 export async function POST(request: Request) {
-  const body = await request.json();
-  const value = body.value ?? 0;
- 
-  function fibonacci(value: number): number {
-    if (value <= 1) return value;
-    return fibonacci(value - 1) + fibonacci(value - 2);
-  }
- 
-  const result = fibonacci(value);
- 
-  return new Response(JSON.stringify({ value, result }), {
-    headers: { 'Content-Type': 'application/json' },
-    status: 200,
-  });
+  const body = await request.json();
+  const value = body.value ?? 0;
+  function fibonacci(value: number): number {
+    if (value <= 1) return value;
+    return fibonacci(value - 1) + fibonacci(value - 2);
+  }
+  const result = fibonacci(value);
+  return new Response(JSON.stringify({ value, result }), {
+    headers: { "Content-Type": "application/json" },
+    status: 200,
+  });
 }
